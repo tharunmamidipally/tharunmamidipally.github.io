f9971bdfdf2b7cde17f319920f3499ecf200e010
tharunmamidipally <128895244+tharunmamidipally@users.noreply.github.com>
2023-06-15 18:09:37 +0530

    Add files via upload

---
+++ backgroundremover/__init__.py
@@ -0,0 +1,9 @@
+"""
+backgroundremover
+
+A library to remove background from videos and images
+"""
+
+__version__ = "0.2.3"
+__author__ = 'Johnathan Nader'
+__credits__ = 'BackgroundRemoverAI.com'
